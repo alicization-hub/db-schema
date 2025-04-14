9c398626e59121c00c425a6ea1f7cca707835782
Sucrose <pinn.project@outlook.com>
2025-04-15 01:39:03 +0700

    Fix: dependencies

--- src/drizzle/index.ts
+++ src/drizzle/index.ts
@@ -2,8 +2,8 @@ import * as accounts from './schema/accounts.schema'
 import * as roles from './schema/roles.schema'
 import * as spaces from './schema/spaces.schema'
 
-export const schema = Object.freeze({
+export const schema = {
   ...accounts,
   ...roles,
   ...spaces
-})
+}
